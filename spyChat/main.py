67d0d56b15723d2afda309b1470c5433ca79f043
nitkgupta <nitkarshg@gmail.com>
2017-08-04 22:31:42 +0530

    spyChat initiated

---
+++ spyChat/main.py
@@ -0,0 +1,5 @@
+print 'Let\'s get Started...'
+spy_name=raw_input('What is your name? : ')
+spy_salutation=raw_input('What should we call you? : ')
+spy_name=spy_salutation + spy_name
+print 'Welcome ' + spy_name + ' Glad to have you back with us.'
